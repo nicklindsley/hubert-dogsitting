57476ea22550258424a572e132358a86a53eb470
nick.lindsley <nick.lindsley@belmark.com>
2025-12-17 23:04:22 -0600

    Style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
 	import { hubertSleeping, hubertDigging } from '$lib/assets';
 </script>
 
-<div class="flex flex-col items-center">
-	<h1>Hubert Dogsitting Guide</h1>
-	<p>Hubbie is a good boy yes he is</p>
+<div class="flex flex-col items-center gap-4">
+	<div class="flex flex-col items-center">
+		<h1 class="font-bold">Hubert Dogsitting Guide</h1>
+		<p>Hubbie is a good boy</p>
+		<p class="text-xs italic">...Except when he gets a pee-paw</p>
+	</div>
 	<img src={hubertSleeping} alt="Hubert sleeping" width={400} />
 	<video controls width={200}>
 		<source src={hubertDigging} />
