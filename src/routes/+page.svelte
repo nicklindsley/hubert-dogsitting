004047870b929cbb2dd80004573c99bcde34eff1
nick.lindsley <nick.lindsley@belmark.com>
2025-12-17 21:19:51 -0600

    Add hubert assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+</script>
+
 <h1>Hubert Dogsitting Guide</h1>
-<p>Hubbie is a good boy</p>
+<p>Hubbie is a good boy yes he is</p>
+<img src="/hubert-sleeping.jpeg" alt="Hubert sleeping" width={800} />
+<video controls width={200}>
+	<source src="/hubert-digging.mp4" />
+</video>
