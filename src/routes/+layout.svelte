f1a8c60759d3f2e16f5132627d76039aaeda7e18
nick.lindsley <nick.lindsley@belmark.com>
2025-12-18 23:06:02 -0600

    Add meta data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,11 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Hubert Dogsitting Guide</title>
+	<meta property="og:image" content={favicon} />
+	<meta property="og:image:width" content="580" />
+	<meta property="og:image:height" content="580" />
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
